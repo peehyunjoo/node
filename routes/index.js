8e676a2ece281e96058658fd92ccc99820786ecf
pizzu <peehyunjoo@gmail.com>
2017-05-14 01:32:08 +0900

    attendance list

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
-
+var attendance = require('./usermodel2');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', id:null });
+        attendance.findAll().then(function(results){
+			results=JSON.parse(JSON.stringify(results));
+			console.log(results);
+		res.render('index',{rows:results,id:null});
+	});
+  //res.render('index', { title: 'Express', id:null });
 });
 router.get('/logout', function(req, res, next) {
   		req.session.destroy(function(err){
